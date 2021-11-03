33f7011af0d0e1f22c835e3917ecfcc1c60c976e
Mehdi Hossain <56967184+MaddyUnknown@users.noreply.github.com>
2021-11-03 15:00:46 +0530

    Initial Commit
    Added file structure for storing common css, js and keeping resources seperately. Also Added Ticket Booking Page (Payment Page)

---
+++ FrontEnd/assets/js/pages/payment.js
@@ -0,0 +1,51 @@
+const range = document.getElementById("num-seats")
+const bubble = document.getElementsByClassName("bubble")[0]
+const submit_btn = document.getElementById("submit-btn")
+
+// ******************************On Load********************************
+window.onload = ()=>{
+    range.value = 1;
+    setBubble(range, bubble)
+}
+
+// ****************************Util Functions******************************
+function setBubble(range, bubble) {
+    const val = range.value;
+    const min = range.min;
+    const max = range.max;
+    const newVal = Number(((val - min) * 89) / (max - min));
+    bubble.innerHTML = val;
+
+     bubble.style.left = `calc(${newVal}% + (23px))`;
+}
+
+
+// ************************** Event Listners **************************
+range.addEventListener("input", () => {
+    setBubble(range, bubble);
+});
+
+submit_btn.addEventListener("click", ()=>{
+    const all_input = Array.from(document.getElementsByTagName("input"))
+    all_input.forEach(element => {
+        // console.log(element.value)
+        const error_message = document.querySelector(`small[data-validator="${element.id}"]`)
+        if(!element.value){
+            error_message.classList.remove("hidden")
+        }
+        else if(element.id === "email" && !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(element.value)){
+            error_message.classList.remove("hidden")
+        }
+        else if(element.id === "card-cvv" && !/^[0-9]{3}$/.test(element.value)){
+            error_message.classList.remove("hidden")
+        }
+        else if(element.id === "card-num" && !/^[0-9]{16}$/.test(element.value)){
+            error_message.classList.remove("hidden")
+        }
+        else if(element.id !== "num-seats"){
+            error_message.classList.add("hidden")
+        }
+    });
+
+
+})
